6b2ac4da958e966380f4ec28c721667f49bff89d
Cristobal Lama <cristoballama@gmail.com>
2013-04-15 10:24:29 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LACUA::Application.config.secret_token = '8a43c46501fc29a6ccb0e40e8eeb1adebe96a4516f2fb70f4a425eba54a6fd52f444e93dea163cc0a40f882027d07dd1654152dd4cda757d62b3e9f495e4ad78'
