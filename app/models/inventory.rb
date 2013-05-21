072764b64dfca8c69e2a17c23b2a0687ec9942a4
Benjamin Barros <benjamin.bga@gmail.com>
2013-05-21 19:44:57 -0400

    agregado metodo para actualizar inventario

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,25 @@
 class Inventory < ActiveRecord::Base
   attr_accessible :amount, :location_id, :sub_location
   has_and_belongs_to_many :clothes
+
+	def withdraw(value)
+		updateAmount(value*-1)
+	end
+
+	def addAmount(value)
+		updateAmount(value)
+	end	
+
+
+	def updateAmount(value)
+		if (amount + value < 0)
+			diference = amount + value
+			write_attribute(:amount, 0)
+			return diference
+		else
+			write_attribute(:amount, amount + value)
+			return amount
+		end
+	end
+
 end
