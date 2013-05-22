e1252242296ac3bc095826e4d365c450dfd2f618
Cristobal Lama <cristoballama@gmail.com>
2013-05-22 14:36:15 -0400

    Revert "agragada la gema rails_admin"
    This reverts commit e72592610bea93a86ac1f754b9b8c79446f9dcab.

--- app/models/cloths_inventory.rb
+++
@@ -1,6 +0,0 @@
-class ClothsInventory < ActiveRecord::Base
-  	attr_accessible :amount, :inventory_id, :cloth_id
-  	
-  	belongs_to :inventory
-	  belongs_to :cloth
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 LACUA::Application.routes.draw do
   
-  
-
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   devise_for :users
