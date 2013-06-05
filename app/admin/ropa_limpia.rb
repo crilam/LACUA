8ef41eb9a46516ec5775a14ed31a456fcc554b81
Cristobal Lama <cristoballama@gmail.com>
2013-06-05 15:24:43 -0400

    Agregada partials de prueba para ropa limpia y ropa sucia

---
+++ app/admin/ropa_limpia.rb
@@ -0,0 +1,63 @@
+ActiveAdmin.register Inventory , :as => "Ropa Limpia" do
+  
+  menu :parent => "Inventario"
+  
+  #filtrar los resultados
+  controller do
+    def scoped_collection
+      Inventory.where(:state => "limpia")
+    end
+  end
+  ## Index ##
+  index do
+    column :descripcion do |inventory|
+            inventory.location.name
+    end
+    column :state
+    default_actions
+  end
+  ##
+  ## Show ##
+  show do
+    panel "Inventaries" do
+      table_for inventory.cloths_inventories do
+        column "Cantidad" do |cloths_inventories|
+          cloths_inventories.amount
+        end
+        column "Distincion" do |cloths_inventories|
+          cloths_inventories.cloth.distintion
+        end
+        column "Alto" do |cloths_inventories|
+          cloths_inventories.cloth.height
+        end
+        column "Ancho" do |cloths_inventories|
+          cloths_inventories.cloth.width
+        end
+        column "Largo" do |cloths_inventories|
+          cloths_inventories.cloth.depth
+        end
+        column "Color" do |cloths_inventories|
+          cloths_inventories.cloth.color
+        end
+        column "Feston" do |cloths_inventories|
+          cloths_inventories.cloth.feston
+        end
+        column "Tipo" do |cloths_inventories|
+          cloths_inventories.cloth.tipo
+        end
+        column "Categoria" do |cloths_inventories|
+          cloths_inventories.cloth.category.name
+        end
+
+      end
+    end
+  end
+  action_item do
+     link_to "Message Customers"
+   end
+  ##
+  ## Form ##
+  form :partial => "register_clean_cloths"
+  ##
+  
+end
