40d26510132850de1573360f61005e585d2af336
Cristobal Lama <cristoballama@gmail.com>
2013-06-05 16:48:03 -0400

    Cmabios en la redireccion principal

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -92,8 +92,8 @@ ActiveAdmin.setup do |config|
   # roots for each namespace.
   #
   # Default:
-  # config.root_to = 'dashboard#index'
-  config.root_to = 'inventories#index'
+   config.root_to = 'dashboard#index'
+  #config.root_to = 'inventories#index'
 
   # == Admin Comments
   #
