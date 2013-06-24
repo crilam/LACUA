de71dc64af1c978f33b8c042089c1316794b178e
Mauricio Vera <mverapino@gmail.com>
2013-06-24 01:28:30 -0400

    algunas modificaciones de vista,al ingresar ropa. Hacer que guarde el movimiento

--- app/admin/ropa_limpia.rb
+++ app/admin/ropa_limpia.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Inventory , :as => "Ropa Limpia" do
-  
+
   menu :parent => "Inventario"
   
   #filtrar los resultados
@@ -55,23 +55,25 @@ ActiveAdmin.register Inventory , :as => "Ropa Limpia" do
   action_item do
      link_to "Registrar ropa Limpia" , register_clean_cloths_admin_ropa_limpium_path($ID_ROPA_LIMPIA)
   end
-  member_action :register_clean_cloths do
-    @cloths = Cloth.all
+    member_action :register_clean_cloths do
+      @cloths = Cloth.all
     end
-    member_action :create_register_clean_cloths do
+    member_action :create_register_clean_cloths,  :method => :post do
       inventory = Inventory.find($ID_ROPA_LIMPIA)
 
       redirect_to register_clean_cloths_path, notice: 'Error en la solicitud' if params[:items].nil?
 
-      params[:items].each do|i|
+      Cloth.transaction       do
+        params[:items].each do|i|
 
-        #inventory = Inventory.find(i[:inventory_id])
-        cloth_inventory = ClothsInventory.where(inventory_id: inventory.id, cloth_id: i[1][:cloth_id]).first
-        if cloth_inventory
-          cloth_inventory.addAmount(i[1][:amount].to_i)
-        else
-          ClothsInventory.new(inventory_id: inventory.id,cloth_id:i[1][:cloth_id], amount:i[1][:amount])
-
+          #inventory = Inventory.find(i[:inventory_id])
+          cloth_inventory = ClothsInventory.where(inventory_id: inventory.id, cloth_id: i[1][:cloth_id]).first
+          if cloth_inventory
+            cloth_inventory.addAmount(i[1][:amount].to_i)
+          else
+            cloth_inventory = ClothsInventory.new(inventory_id: inventory.id,cloth_id:i[1][:cloth_id], amount:i[1][:amount])
+          end
+          cloth_inventory.save!
         end
       end
       redirect_to admin_ropa_limpia_path, notice: 'Ropa ingresada'
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,5 @@
 //= require active_admin/base
+//= require jquery_ujs
+//= require jquery-tablesorter
+//= require jquery.tablesorter.scroller.js
+
